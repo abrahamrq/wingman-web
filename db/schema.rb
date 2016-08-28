c5bb3072935c1638023fc6a7158f0c95425fd052
Marcelo <salcedogzz.jm@gmail.com>
2016-08-28 07:41:52 -0500

    Added detail to landing page, login modal, profile show, navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160828084322) do
-=======
-ActiveRecord::Schema.define(version: 20160828053518) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
